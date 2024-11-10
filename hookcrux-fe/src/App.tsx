85540a69c3177e4d695df913d7f75d0c94b1d236
Andrej Mokris <mokris.andrej@gmail.com>
2024-11-11 00:51:18 +0100

    add theming to FE app

--- hookcrux-fe/src/App.tsx
+++ hookcrux-fe/src/App.tsx
@@ -1,24 +1,27 @@
 import { Route, Routes } from 'react-router-dom';
 import { Footer } from './components/footer';
 import { Header } from './components/header';
+import { ThemeProvider } from './components/theme-provider';
 import { LandingPage } from './pages/landing-page';
 import { SessionPage } from './pages/session-page';
 
 export default function App() {
   return (
-    <div className="min-h-screen flex flex-col bg-black text-white overflow-hidden">
-      <div className="absolute inset-0 bg-[radial-gradient(ellipse_at_center,_var(--tw-gradient-stops))] from-blue-900 via-black to-black opacity-50"></div>
+    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+      <div className="min-h-screen flex flex-col bg-black text-white overflow-hidden">
+        <div className="absolute inset-0 bg-[radial-gradient(ellipse_at_center,_var(--tw-gradient-stops))] from-blue-900 via-black to-black opacity-50"></div>
 
-      <Header />
+        <Header />
 
-      <main className="flex-grow relative z-10">
-        <Routes>
-          <Route path="/" Component={LandingPage} />
-          <Route path="/session/:id" Component={SessionPage} />
-        </Routes>
-      </main>
+        <main className="flex-grow relative z-10">
+          <Routes>
+            <Route path="/" Component={LandingPage} />
+            <Route path="/session/:id" Component={SessionPage} />
+          </Routes>
+        </main>
 
-      <Footer />
-    </div>
+        <Footer />
+      </div>
+    </ThemeProvider>
   );
 }
